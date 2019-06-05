4ee3bd9047932089456664ea9e9e227f054884f9
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-06-05 16:35:16 -0700

    refactored factorial funct

--- Code/recursion.py
+++ Code/recursion.py
@@ -18,12 +18,8 @@ def factorial_iterative(n):
     if n == 0 or n == 1:
         return 1
     elif n > 1:
-        for i in range(1, n+1):
-            factorial = factorial * n
+        factorial = factorial * n
     return factorial
-    # once implemented, change factorial (above) to call factorial_iterative
-    # to verify that your iterative implementation passes all tests
-
 
 def factorial_recursive(n):
     # check if n is one of the base cases
