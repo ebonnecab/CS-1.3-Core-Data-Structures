b2e0550a7cdbd2e0c84aeb65fcc60e8507bebce2
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-06-09 20:44:48 -0700

    recursive linear test passing

--- Code/search.py
+++ Code/search.py
@@ -5,8 +5,8 @@ def linear_search(array, item):
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
 
-    return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+    # return linear_search_iterative(array, item)
+    return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -19,16 +19,13 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     
-    if array[0] == item:
-        return 0
-    
-    elif array[1] == item:
-        return 1
+    if index > len(array)-1:
+        return None
 
     elif array[index] == item:
         return index
-
-    return linear_search_recursive(array, item, index+1)
+    else:
+        return linear_search_recursive(array, item, index+1)
             
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
@@ -38,8 +35,8 @@ def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
     # implement binary_search_iterative and binary_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return binary_search_iterative(array, item)
-    return binary_search_recursive(array, item)
+    return binary_search_iterative(array, item)
+    # return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
