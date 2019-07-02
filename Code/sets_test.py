d60b3b9bb08ccff57af2753f9300891288e5273f
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-07-02 14:49:25 -0700

    added unit test for size

--- Code/sets_test.py
+++ Code/sets_test.py
@@ -11,6 +11,16 @@ class SetSetTest(unittest.TestCase):
         array_set = SetSet(['A','B', 'D', 'F'])
         assert array_set.size == 4
         assert array_set.__contains__('D') is True
+    
+    def test_size(self):
+        array_set = SetSet()
+        assert array_set.size == 0
+        array_set.__add__(2)
+        assert array_set.size == 1
+        array_set.__add__(3)
+        assert array_set.size == 2
+        array_set.__remove__(3)
+        assert array_set.size == 1
 
 if __name__ == '__main__':
     unittest.main()
