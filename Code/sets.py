bfe7961d13989e2cd58a2a45ef8209995630fcea
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-07-01 21:29:05 -0700

    added methods for set class

--- Code/sets.py
+++ Code/sets.py
@@ -1,2 +1,27 @@
+# from linkedlist import LinkedList
 class SetSet:
+    def __init__(self, elements=None):
+        self.size = 0
+        self.elements = list()
+        
+        if elements is not None:
+            for element in elements:
+                self.add(element)
+    
+    def __contains__(self, element):
+        return True if element in self.elements else False
+    
+    def __add__(self, element):
+        if not self.contains(element):
+            self.elements.append(element)
+            self.size += 1
+
+    def __remove__(self, element):
+        if self.contains(element):
+            self.elements.remove(element)
+            self.size -= 1
+        else:
+            raise KeyError('element not found')
+    
+    
 
