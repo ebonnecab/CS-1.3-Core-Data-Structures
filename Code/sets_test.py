d715dc802a9866409d67e25dfd73a013209f7178
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-07-02 15:00:54 -0700

    passing tests for remove method

--- Code/sets_test.py
+++ Code/sets_test.py
@@ -42,5 +42,21 @@ class SetSetTest(unittest.TestCase):
         assert array_set.__contains__('F')
         array_set.__remove__('E')
         assert not array_set.__contains__('E')
+    
+    def test_remove(self):
+        array_set = SetSet()
+        array_set.__add__(2)
+        array_set.__add__(3)
+        array_set.__add__(100)
+        array_set.__remove__(100)
+        assert not array_set.__contains__(100)
+        assert array_set.size == 2
+        array_set.__remove__(2)
+        assert not array_set.__contains__(2)
+        assert array_set.size == 1
+        array_set.__remove__(3)
+        assert not array_set.__contains__(3)
+        assert array_set.size == 0
+        
 if __name__ == '__main__':
     unittest.main()
